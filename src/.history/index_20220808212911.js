f5e5359a65f2bbb7c7e96dca80b4557900b9ffb2
EgorSherstnev <svarog91@list.ru>
2022-08-08 21:38:19 +0300

    add_css_and_js_react

--- src/.history/index_20220808212911.js
+++ src/.history/index_20220808212911.js
@@ -1,7 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-
 class Square extends React.Component {
   render() {
     return <button className="square">{/* TODO */}</button>;
